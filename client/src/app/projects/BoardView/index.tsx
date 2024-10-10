b87daad87eda3a59aac069358c28b17ec45a3412
Adrian Jazmin <adriancoderajazmin@gmail.com>
2024-10-10 11:07:20 +0800

    image redirect

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -184,7 +184,7 @@ const Task = ({ task }: TaskProps) => {
       >
         {task.attachments && task.attachments.length > 0 && (
           <Image
-            src={`/${task.attachments[0].fileURL}`}
+            src={`https://taskit-s3-pics.s3.amazonaws.com/${task.attachments[0].fileURL}`}
             alt={task.attachments[0].fileName}
             width={400}
             height={200}
@@ -233,7 +233,7 @@ const Task = ({ task }: TaskProps) => {
               {task.assignee && (
                 <Image
                   key={task.assignee.userId}
-                  src={`/${task.assignee.profilePictureUrl!}`}
+                  src={`https://taskit-s3-pics.s3.amazonaws.com/${task.assignee.profilePictureUrl!}`}
                   alt={task.assignee.username}
                   width={30}
                   height={30}
@@ -243,7 +243,7 @@ const Task = ({ task }: TaskProps) => {
               {task.author && (
                 <Image
                   key={task.author.userId}
-                  src={`/${task.author.profilePictureUrl!}`}
+                  src={`https://taskit-s3-pics.s3.amazonaws.com/${task.author.profilePictureUrl!}`}
                   alt={task.author.username}
                   width={30}
                   height={30}
