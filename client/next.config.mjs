1d9ea811012c91934cbb77a4aac990a082b088c7
Adrian Jazmin <adriancoderajazmin@gmail.com>
2024-10-10 11:12:21 +0800

    images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "taskit-s3-pics.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
+};
 
 
 export default nextConfig;
